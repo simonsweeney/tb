5851723837d25f8df8d91f0ef10431260256207d
simonsweeney <hello@simonsweeney.me>
2014-08-07 12:49:42 +0100

    card shuffling and bg colour js wizardy added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,39 @@
-/*$(function() {                      
-  $("#hS").hover(function() {  
-    $("#menuOne").toggleClass("top"); 
-  });
-  $("#bW").hover(function() {  
-    $("#menuTwo").toggleClass("top"); 
-  });
-  $("#cHq").hover(function() {  
-    $("#menuThree").toggleClass("top"); 
-  });
-  $("#c").hover(function() {  
-    $("#menuFour").toggleClass("top"); 
-  });
-});*/
+var $cards = $('.menu');
+
+var highestZ = $cards.length;
+
+function bringToFront(i){
+  //$cards.removeClass('front');
+  //$cards.eq(i).addClass('front');
+  $cards.eq(i).css('z-index', ++highestZ);
+  console.log(1);
+}
+
+$cards.each(function(i){
+  $(this).on('mouseenter', function(){
+    bringToFront(i);
+  })
+});
+
+$('.name').each(function(i){
+  $(this).on('click', function(){
+    bringToFront(i);
+  })
+});
 
 
-	$('#menuOne').on('mouseenter', function(){
-	    $("#menuOne").toggleClass("top"); 
-	});
+function bgPink() {
+	$("#bg").css('background', '#F5E0F2');
+}
+
+function bgBlue() {
+	$("#bg").css('background', '#DDEAF4');
+}
+
+$('#bgBlue').click(function() {bgBlue();});
+$('#bgPink').click(function() {bgPink();});
+
+$('#menuOne').on('mouseenter', function(){bgBlue();});
+$('#menuTwo').on('mouseenter', function(){bgBlue();});
+$('#menuThree').on('mouseenter', function(){bgPink();});
+$('#menuFour').on('mouseenter', function(){bgPink();});
