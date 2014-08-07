d791f10afd30e2dfce4862bdb006e2abb349d46f
simonsweeney <hello@simonsweeney.me>
2014-08-07 02:13:32 +0100

    inital structures

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+/*$(function() {                      
+  $("#hS").hover(function() {  
+    $("#menuOne").toggleClass("top"); 
+  });
+  $("#bW").hover(function() {  
+    $("#menuTwo").toggleClass("top"); 
+  });
+  $("#cHq").hover(function() {  
+    $("#menuThree").toggleClass("top"); 
+  });
+  $("#c").hover(function() {  
+    $("#menuFour").toggleClass("top"); 
+  });
+});*/
+
+
+	$('#menuOne').on('mouseenter', function(){
+	    $("#menuOne").toggleClass("top"); 
+	});
